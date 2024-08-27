8b78b8cfa15f87518b8a47f7e3f4d35134782bc4
Gabriel <gabrielleonpineros@gmail.com>
2024-08-27 16:51:14 -0500

    Version Final 1.1.0

---
+++ tabla.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+);
